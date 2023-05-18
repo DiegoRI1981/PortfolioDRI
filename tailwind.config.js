db844333c0d5b24cf77f33bd8bf232e46445bfc7
Diego Rodriguez <godiedri@gmail.com>
2023-05-18 16:35:30 +0200

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,7 @@ module.exports = {
             "color-gray": "#333",
             "color-white": "#fff",
             "color-envelope": "#ff0000",
+            "color-primary-light": "#ecedf0",
         },
 
         container: {
